152c2076071e7540c392484813caa64a32f6f7a2
Flalufdz <flaviaivanafernandez@gmail.com>
2020-08-09 17:57:35 -0300

    Binarios random

--- js/respuestas.js
+++ js/respuestas.js
@@ -1,4 +1,24 @@
-
+function generarNro(){
+  return Math.floor(Math.random() * 12);
+}
+function generarRandom(){
+  let binarios =["1000100010010000","0000000000000000","1111111111111111","0101010101010101","1000010000000101",
+                  "0100001111101000","1100000010000000","1010100010000000","1000000000001000", "0000000001000001",
+                    "0100001001011100","0000000000000001"];
+ alert('Random anda');
+ document.getElementById('operator1').value=binarios[generarNro()];
+ document.getElementById('operator2').value=binarios[generarNro()];
+ document.getElementById('operator3').value=binarios[generarNro()];
+ document.getElementById('operator4').value=binarios[generarNro()]; 
+ document.getElementById('operator5').value=binarios[generarNro()]; 
+ document.getElementById('operator6').value=binarios[generarNro()]; 
+ document.getElementById('operator7').value=binarios[generarNro()]; 
+ document.getElementById('operator8').value=binarios[generarNro()]; 
+ document.getElementById('operator9').value=binarios[generarNro()]; 
+ document.getElementById('operator10').value=binarios[generarNro()]; 
+ document.getElementById('operator11').value=binarios[generarNro()]; 
+ document.getElementById('operator12').value=binarios[generarNro()]; 
+}
 function corregirAnd(operator1,operator2,result) {
 	alert("se ejecuto la funcion");
   var rtaOperacion=0;
@@ -25,6 +45,8 @@ function corregirAllAnd(){
   corregirAnd('operator3','operator4','result2');
   corregirAnd('operator5','operator6','result3');
   corregirAnd('operator7', 'operator8','result4');
+  corregirAnd('operator9', 'operator10','result5');
+  corregirAnd('operator11', 'operator12','result6');
 }
 function corregirOr(operator1,operator2,result,msjOk,msjError) {
 	alert("se ejecuto la funcion");
