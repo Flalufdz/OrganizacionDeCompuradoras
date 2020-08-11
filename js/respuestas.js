4c7fe28024db64fe01267ef65f0a3b07fb56ced9
Flalufdz <flaviaivanafernandez@gmail.com>
2020-08-10 21:04:22 -0300

    Refactor EjerciciosOr

--- js/respuestas.js
+++ js/respuestas.js
@@ -5,7 +5,7 @@ function generarRandom(){
   let binarios =["1000100010010000","0000000000000000","1111111111111111","0101010101010101","1000010000000101",
                   "0100001111101000","1100000010000000","1010100010000000","1000000000001000", "0000000001000001",
                     "0100001001011100","0000000000000001"];
- alert('Random anda');
+
  document.getElementById('operator1').value=binarios[generarNro()];
  document.getElementById('operator2').value=binarios[generarNro()];
  document.getElementById('operator3').value=binarios[generarNro()];
@@ -19,7 +19,17 @@ function generarRandom(){
  document.getElementById('operator11').value=binarios[generarNro()]; 
  document.getElementById('operator12').value=binarios[generarNro()]; 
 }
-function corregirAnd(operator1,operator2,result) {
+
+function calcAnd(operando1, operando2){
+ return operando1 & operando2;
+} 
+
+function calcOr(operando1, operando2){
+  return operando1 | operando2;
+}
+
+
+function corregir(operator1,operator2,result, calc) {
 	alert("se ejecuto la funcion");
   var rtaOperacion=0;
   var operando1=0;
@@ -30,44 +40,36 @@ function corregirAnd(operator1,operator2,result) {
    operando1=parseInt(operandos1,2);
    operando2=parseInt(operandos2,2);
    rtaAlumno=parseInt(rtaAlumnos.value,2);
-   rtaOperacion=operando1 & operando2;
+   rtaOperacion= calc(operando1, operando2);
    if(rtaAlumno == rtaOperacion){
-     alert('bien');
     rtaAlumnos.classList.add('is-valid');
   }else{
-    alert(mal);
     rtaAlumnos.classList.add('is-invalid');
   }
 }
 
-function corregirAllAnd(){
-  corregirAnd('operator1','operator2','result1');
-  corregirAnd('operator3','operator4','result2');
-  corregirAnd('operator5','operator6','result3');
-  corregirAnd('operator7', 'operator8','result4');
-  corregirAnd('operator9', 'operator10','result5');
-  corregirAnd('operator11', 'operator12','result6');
-}
-function corregirOr(operator1,operator2,result,msjOk,msjError) {
-	alert("se ejecuto la funcion");
-  var rtaOperacion=0;
-  var operando1=0;
-  var operando2=0;
-  var operandos1=document.getElementById(operator1).value;
-  var operandos2=document.getElementById(operator2).value; 
-  var rtaAlumnos =document.getElementById(result).value; 
-   operando1=parseInt(operandos1,2);
-   operando2=parseInt(operandos2,2);
-   rtaAlumno=parseInt(rtaAlumnos,2);
-   alert("op1 "+operando1+", op2 "+operando2+" = "+rtaAlumnos);
-   rtaOperacion=operando1 | operando2;
-   alert(rtaAlumno+" = "+rtaOperacion);
-  mostrarMensaje(rtaAlumno,rtaOperacion,msjOk,msjError);
+function corregirAll(calc){
+  corregir('operator1','operator2','result1',calc);
+  corregir('operator3','operator4','result2',calc);
+  corregir('operator5','operator6','result3',calc);
+  corregir('operator7', 'operator8','result4',calc);
+  corregir('operator9', 'operator10','result5',calc);
+  corregir('operator11', 'operator12','result6',calc);
 }
 
+
 function corregirAllOr(){
-  corregirOr('operator1','operator2','result1','msjOk1','msjError1');
-  corregirOr('operator3','operator4','result2','msjOk2','msjError2');
-  corregirOr('operator5','operator6','result3','msjOk3','msjError3');
-  corregirOr('operator7', 'operator8','result4','msjOk4','msjError4');
+  corregirAll(calcOr);
+  alert('Las respuestas marcadas en Verde son Correctas, las marcadas en Rojo incorrectas');
+  if (window.history.replaceState){
+    window.history.replaceState(null, null, window.location.href);
+  }
+}
+
+function corregirAllAnd(){
+  corregirAll(calcAnd);
+  alert('Las respuestas marcadas en Verde son Correctas, las marcadas en Rojo incorrectas');
+  if (window.history.replaceState){
+    window.history.replaceState(null, null, window.location.href);
+  }
 }
